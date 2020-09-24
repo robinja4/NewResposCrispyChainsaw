84ffed6d96ef1cdbfa20bae1880c99a7a4c598f1
Tyler Perkins (10007440) <PERKITW2@student.op.ac.nz>
2020-09-24 15:51:16 +1200

    kdsdfsdfsdfs

--- GithubCrispyChainsaw/GithubCrispyChainsaw/Program.cs
+++ GithubCrispyChainsaw/GithubCrispyChainsaw/Program.cs
@@ -4,9 +4,23 @@ namespace GithubCrispyChainsaw
 {
     class Program
     {
-        static void Main(string[] args)
+       
+        static void Question2()
         {
+            Random rand = new Random();
+            string temp;
+            int input
             Console.WriteLine("Hello World!");
+        }
+
+        static void Question3()
+        {
+            string temp;
+            Console.WriteLine("What is the original name for Myanmar?");
+            temp = Console.ReadLine();
+
+
+            
         }
     }
 }
