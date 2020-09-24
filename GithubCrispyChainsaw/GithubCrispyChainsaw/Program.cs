8a8e878b864557cff2ac6d38469cd3f23561d78b
Jamie Robinson (08001072) <ROBINJA4@student.op.ac.nz>
2020-09-24 15:52:55 +1200

    This will commit this code

--- GithubCrispyChainsaw/GithubCrispyChainsaw/Program.cs
+++ GithubCrispyChainsaw/GithubCrispyChainsaw/Program.cs
@@ -4,9 +4,19 @@ namespace GithubCrispyChainsaw
 {
     class Program
     {
+        string answer
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        { 
+           
+            Console.WriteLine("Welcome to the Question Game");
+
+            do
+            {
+                Console.WriteLine("What is 5x5?");
+                answer = Console.ReadLine();
+                
+
+            } while (answers < 3);
         }
     }
 }
